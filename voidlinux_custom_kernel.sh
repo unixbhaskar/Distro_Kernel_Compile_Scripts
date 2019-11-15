d796d0b691919f268ddfb7e8fb74c01e988c656b
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2019-11-15 13:06:25 +0530

    well,it is ugly to the best, but it works :)
    Signed-off-by: Bhaskar Chowdhury <unixbhaskar@gmail.com>

--- voidlinux_custom_kernel.sh
+++ voidlinux_custom_kernel.sh
@@ -22,14 +22,28 @@ printf "${Bright}${Red}Not enough space...aborting! ${NOCOLOR} \n\n\n\n"
 exit 1
 fi
 
-kernel=$(curl -s https://www.kernel.org/ | grep -A1 'stable:' | grep -oP '(?<=strong>).*(?=</strong.*)' | grep 5.3)
+kernel=$(curl -s https://www.kernel.org/ | grep -A1 'stable:' | grep -oP '(?<=strong>).*(?=</strong.*)' | grep 5.2) 
+printf "${Bright}${Green}$kernel${NOCOLOR} \n"
 
-printf "${Bright}${Cyan} $kernel ${NOCOLOR} \n\n"
 
-#standard way of installing distro kernel
-printf "\n\n ${Reverse}${Bright}${Green} ################ Updating the KERNEL.... ################# ${NOCOLOR} \n\n"
+avil_kernel=$(xbps-query --regex -Rs '^linux[0-9.]+-[0-9._]+' | tail -n 1)
 
-  xbps-install -S --yes liux5.3
+printf "${Bright}${Cyan} The latest stable kernel is $avil_kernel ${NOCOLOR} \n\n"
+
+installed_kernel=$(uname -a | gawk '{ print $3 }')
+
+printf "${bright}${Cyan} Installed kernel $installed_kernel ${NOCOLOR} \n\n"
+#if [[ "$avil_kernel" -ne "$installed_kernel" ]];then
+
+ #    printf "${Bright}${Green} Please proceed with new kernel install. ${NOCOLOR} \n\n"
+#else
+#    printf "${Bright}${Red} Updted kernel not available. ${NOCOLOR} \n\n"
+#    exit 1
+#fi    
+
+printf "\n\n ${Reverse}${Bright}${Green} ################ Updating the KERNEL ################# ${NOCOLOR} \n\n"
+
+  xbps-install -S --yes linux5.3
 
 
 printf "${Bright}${Green} Fix the boot entry ....${NOCOLOR} \n\n"
@@ -37,10 +51,10 @@ printf "${Bright}${Green} Fix the boot entry ....${NOCOLOR} \n\n"
 
 cd /boot
 
-ln -s  vmlinuz-$kernel_2  vmlinuz-$kernel-$(hostname)
+ln -s  vmlinuz-"$kernel"_1  vmlinuz-$kernel-$(hostname)
 cp -v vmlinuz-$kernel-$(hostname)  $EFIBOOTDIR
 
-ln -s initramfs-$kernel_2.img  initramfs-$kernel-$(hostname).img
+ln -s initramfs-"$kernel"_1.img  initramfs-$kernel-$(hostname).img
 cp -v initramfs-$kernel-$(hostname).img  $EFIBOOTDIR
 
 
