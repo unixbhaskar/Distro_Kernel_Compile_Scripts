aa0e87e8f445541d42026cb384bd49d495a00b75
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2019-11-15 13:03:18 +0530

    its ugly but it works,latest one :)
    Signed-off-by: Bhaskar Chowdhury <unixbhaskar@gmail.com>

--- archlinux_custom_kernel.sh
+++ archlinux_custom_kernel.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 #This script information based on this wiki page : https://wiki.archlinux.org/index.php/Kernels/Arch_Build_System
 
@@ -16,7 +16,6 @@ printf "Hostname: %s\nDate    : %s\nUptime  :%s\n\n"  "$(hostname -s)" "$(date)"
 
 
 printf "Get the latest kernel version from ${Blue}kernel.org \n\n\n"
-
 kernel=$(curl -s https://www.kernel.org/ | grep -A1 'stable:' | grep -oP '(?<=strong>).*(?=</strong.*)' | grep 5.3)
 printf "${Bright}${GREEN}$kernel${NOCOLOR} \n"
 
@@ -53,25 +52,31 @@ cd linux/repos/core-x86_64/
 
 sudo zcat /proc/config.gz > config
 
-sed -i 's/pkgbase=linux/#pkgbase=linux/' PKGBUILD
+#sed -i 's/pkgbase=linux/#pkgbase=linux/' PKGBUILD
 
+sed -i "s/pkgbase=linux/pkgbase=$(hostname)-$(echo $kernel) /"  PKGBUILD
+pkgver=$(grep "pkgver" PKGBUILD | head -1)
+sed -i "s/$(echo $pkgver)/pkgver=$(echo $kernel) /" PKGBUILD
 
-sed -i "/pkgbase=linux-custom/s/^#/pkgbase=$(hostname)-$(echo $kernel) /"  PKGBUILD
+#sed -i "/pkgbase=linux-custom/s/^#/pkgbase=$(hostname)-$(echo $kernel) /"  PKGBUILD
 
-srcver=$(grep "_srcver=" PKGBUILD)
+#srcver=$(grep "_srcver=" PKGBUILD | head -1)
 
-sed -i "s/$(echo $srcver)/_srcver=$(echo $kernel)-arch1 /" PKGBUILD
-
+sed -i '6d' PKGBUILD
+sed -i '6i _srcver=${pkgver%%%.*}-arch1 '  PKGBUILD
+sed -i '7d' PKGBUILD
 #cn=$(echo $kernel | cut -d"." -f1-2)
 
-sed -i "s/_srcname=archlinux-linux/_srcname=$(echo $kernel)-arch1 /" PKGBUILD
+sed -i '10i _srcname=${pkgver%%%.*}-arch1'  PKGBUILD
 
 #fixed_url="\"$_srcname::git+https://git.archlinux.org/linux.git?signed#tag=v$_srcver\""
 #actual_url="\"\$_srcname::https://git.archlinux.org/linux.git/snapshot/\$_srcver.tar.xz\""
-
-sed -i '20d' PKGBUILD
-sed -i '20i \"$_srcname::https://git.archlinux.org/linux.git/snapshot/\$_srcver.tar.xz\"' PKGBUILD
-
+sed -i '14d' PKGBUILD
+sed -i '16d' PKGBUILD
+sed -i '17d' PKGBUILD
+sed -i '18i \"$_srcname::https://git.archlinux.org/linux.git/snapshot/\$_srcver.tar.gz\"' PKGBUILD
+sed -i '28d' PKGBUILD
+sed -i '28i export KBUILD_BUILD_HOST=Bhaskar_ThinkPad_x250' PKGBUILD
 #sed -i  "s/$(echo $fixed_url)/$(echo $actual_url) /" PKGBUILD
 
 #patch_version=$(grep "pkgver=" PKGBUILD)
@@ -79,6 +84,8 @@ sed -i '20i \"$_srcname::https://git.archlinux.org/linux.git/snapshot/\$_srcver.
 #sed -i "s/$(echo $patch_version)/pkgver=$(echo $kernel) /" PKGBUILD
 
 sed -i 's/#make oldconfig/make olddefconfig/' PKGBUILD
+sed -i '59d' PKGBUILD
+sed -i '59i make -j4 bzImage modules htmldocs' PKGBUILD
 
 
 printf "As we have change the PKGBUILD file ,we need to generate the new ${Magenta}CHECKSUM the file .... ${NOCOLOR} \n\n\n"
@@ -90,17 +97,17 @@ updpkgsums
 
 printf "\n\n\n Lets do the ${Bright}${Green}compiling now ${NOCOLOR} ....\n\n\n"
 
-$TM "\t\n\n Elapsed Time : %E \n\n"  makepkg -s
+$TM "\t\n\n Elapsed Time : %E \n\n"  unbuffer makepkg -s  |  /usr/bin/ts 
 
 
 printf "Install the generated ${PowderBlue}headers,${PowderBlue}kernel and ${PowderBlue}doc packages with pacman .. ${NOCOLOR} \n\n\n"
 
 
-sudo pacman -U  --noconfirm $(hostname)-$kernel-$kernel.arch1-1-x86_64.pkg.tar.xz  
+sudo pacman -U  --noconfirm $(hostname)-$kernel-$kernel-1-x86_64.pkg.tar.xz  
+                             
+sudo pacman -U --noconfirm $(hostname)-$kernel-headers-$kernel-1-x86_64.pkg.tar.xz
 
-sudo pacman -U --noconfirm $(hostname)-$kernel-headers-$kernel.arch1-1-x86_64.pkg.tar.xz
-
-sudo pacman -U  --noconfirm $(hostname)-$kernel-docs-$kernel.arch1-1-x86_64.pkg.tar.xz 
+sudo pacman -U  --noconfirm $(hostname)-$kernel-docs-$kernel-1-x86_64.pkg.tar.xz 
 
 
 printf "\n\n\n Done..now copy over the image to ${Yellow}EFI dir..${NOCOLOR} \n\n\n\n"
@@ -111,15 +118,14 @@ sudo cp -v /boot/initramfs-$(hostname)-$kernel.img $EFIBOOTDIR
 
 printf "${Bright}${Blue}Fixed the boot entry now ${NOCOLOR}...\n\n\n\n"
 
->$EFIBOOTENTRY/ArchLinux.conf
-echo "title ArchLinux" | sudo tee -a $EFIBOOTENTRY/ArchLinux.conf 
+echo "title ArchLinux" | sudo tee  $EFIBOOTENTRY/ArchLinux.conf 
 echo "linux /EFI/ArchLinux/vmlinuz-$(hostname)-$kernel" | sudo tee -a $EFIBOOTENTRY/ArchLinux.conf
-echo "initrd /EFI/ArchLinux/initramfs-$(hostname)-$kernel.img" | sudo tee -a  $EFIBOOTENTRY/ArchLinux.conf
+echo "initrd /EFI/ArchLinux/initramfs-$(hostname)-$kernel.img" | sudo tee -a $EFIBOOTENTRY/ArchLinux.conf
 echo "options root=PARTUUID=e79c9191-3302-4dc8-a62a-5cc22d266643  loglevel=3  systemd.show_status=true rw" | sudo tee -a $EFIBOOTENTRY/ArchLinux.conf 
 
 printf "\n\n\n ${Bright}${Green} Modified the UEFI script... ${NOCOLOR} \n\n"
 
-"\EFI\ArchLinux\vmlinuz-$(hostname)-$kernel  root=PARTUUID=e79c9191-3302-4dc8-a62a-5cc22d266643  loglevel=3  systemd.show_status=true rw initrd=\EFI\ArchLinux\initramfs-$(hostname)-$kernel.img" | sudo tee -a /boot/efi/EFI/archlinux.nsh
+echo "\EFI\ArchLinux\vmlinuz-$(hostname)-$kernel  root=PARTUUID=e79c9191-3302-4dc8-a62a-5cc22d266643  loglevel=3  systemd.show_status=true rw initrd=\EFI\ArchLinux\initramfs-$(hostname)-$kernel.img" | sudo tee  /boot/efi/EFI/archlinux.nsh
 
 exit 0
 
