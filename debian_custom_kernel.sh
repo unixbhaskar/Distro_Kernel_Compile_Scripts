76589ac1d84def4503e62f20ec050effda8acade
Bhaskar Chowdhury <unixbhaskar@gmail.com>
2021-01-13 14:08:16 +0530

    Modified the script with clear inbuilt command
    Signed-off-by: Bhaskar Chowdhury <unixbhaskar@gmail.com>

--- debian_custom_kernel.sh
+++ debian_custom_kernel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-source $HOME/colors.sh
+source /home/bhaskar/colors.sh
 NOCOLOR="\033[0m"
 #DT=`date '+%d%m%Y'`
-BASEPATH=$HOME/latest_kernel_build_$(hostname)_$(date '+%F')
+BASEPATH=/home/bhaskar/latest_kernel_build_$(hostname)_$(date '+%F')
 EFIBOOTDIR=/boot/efi/EFI/debian/
 EFIMENUENTRY=/boot/efi/loader/entries/Debian.conf
-get_kernel=/usr/local/bin/secure_kernel_tarball 
+get_kernel=/usr/local/bin/secure_kernel_tarball
 
 printf "\n\n ${Reverse}################ Building Custom Kernel ${NOCOLOR}################ \n\n"
 
@@ -20,7 +20,7 @@ printf "Hostname: %s\nDate    : %s\nUptime  :%s\n\n"  "$(hostname -s)" "$(date)"
 
 printf " Check the latest stable kernel version from ${Bright}${Blue}kernel.org${NOCOLOR} \n\n"
 #kernel=`curl -sL https://www.kernel.org/finger_banner | grep '4.18' | awk -F: '{gsub(/ /,"", $0); print $2}'`
-kernel=`curl -s https://www.kernel.org/ | grep -A1 'stable' | grep -oP '(?<=strong>).*(?=</strong.*)' | grep 5.3` 
+kernel=`curl -s https://www.kernel.org/ | grep -A1 'stable' | grep -oP '(?<=strong>).*(?=</strong.*)' | grep 5.10`
 printf "${Bright}${Green}$kernel${NOCOLOR}\n\n"
 
 
@@ -46,7 +46,7 @@ eval ${get_kernel} ${kernel}
 
 printf "${Bright}${LimeYellow}Decompress the kernel source ${NOCOLOR}..\n\n"
 
-tar -xJvf linux-$kernel.tar.xz | /usr/local/bin/lolcat
+tar -xJvf linux-$kernel.tar.xz
 
 #printf "${Bright}${Magenta}Verify the GPG signature on the kernel file${NOCOLOR}...\n\n"
 
@@ -67,32 +67,28 @@ printf "${Bright}${PowderBlue}Create a .config file,copying over the existing on
 cp -v /boot/config-`uname -r` .config
 
 
-printf "${Bright}${Blue}Run make olddefconfig on it ${NOCOLOR}....\n\n"
-
-make olddefconfig  | /usr/local/bin/lolcat
-
 
 printf "${Bright}${Red}Taking away faulty option for custom kernel build${NOCOLOR}..\n\n"
 
-
-sed -i 's/CONFIG_SYSTEM_TRUSTED_KEYS=yes/CONFIG_SYSTEM_TRUSTED_KEYS=no /' .config
-
+scripts/config --disable system_trusted_keys
 
 grep CONFIG_SYSTEM_TRUSTED_KEYS .config
 
-sleep 5
+
+printf  "\n${Yellow} Disabling DEBUG INFO${NOCOLOR} . . .\n\n"
+
+scripts/config --disable DEBUG_INFO
+
+printf "${Green}Done${NOCOLOR}\n\n"
+
+printf "${Bright}${Blue}Run make olddefconfig on it ${NOCOLOR}....\n\n"
+
+make olddefconfig
+
 
 printf "${Bright}${Cyan}######################### Lets build the kernel #####################################${NOCOLOR}....\n\n\n"
 
-
-printf  "\n${Yellow} Disabling DEBUG INFO${NOCOLOR} . . .\n\n"
-scripts/config --disable DEBUG_INFO 
-printf " \_ ${Green}Done${NOCOLOR}\n\n"
-
-
-/usr/bin/time -f "\n\n\tTime Elapsed: %E\n\n" unbuffer  fakeroot make-kpkg -j `getconf _NPROCESSORS_ONLN`  --initrd --append-to-version=-`hostname` kernel_image kernel_headers | /usr/bin/ts | /usr/local/bin/lolcat
-
-
+/usr/bin/time -f "\n\n\tTime Elapsed: %E\n\n" make -j `getconf _NPROCESSORS_ONLN` deb-pkg
 
 printf "\n\n\n ${Bright}${Green}Install the generated packages aka kernel,headers,modules et al ${NOCOLOR}\n\n\n"
 
@@ -114,7 +110,7 @@ printf "\n\n\n${Bright}${PowderBlue} Fix the boot entry ${NOCOLOR}...\n\n\n"
 echo "title Debian" > $EFIMENUENTRY
 echo "linux /EFI/debian/vmlinuz-$kernel-debian" >> $EFIMENUENTRY
 echo "initrd /EFI/debian/initrd.img-$kernel-debian" >> $EFIMENUENTRY
-echo "options root=PARTUUID=38fa068d-df77-4562-926c-cbfe8f6c8a52  loglevel=3  systemd.show_status=true rw" >> $EFIMENUENTRY
+echo "options root=PARTUUID=ad5ef658-ccc9-46a5-8363-107a8e5e7d15  loglevel=3  systemd.show_status=true ifname.net=0 rw" >> $EFIMENUENTRY
 
 printf "\n\n${Bright}${Green}Take a look at it ${NOCOLOR}...\n\n\n"
 
@@ -122,7 +118,7 @@ cat $EFIMENUENTRY
 
 printf "\n\n ${Bright}${Cyan} Fix the UEFI boot shell script ... ${NOCOLOR} \n\n"
 
-echo " \EFI\debian\vmlinuz-$kernel-debian root=PARTUUID=38fa068d-df77-4562-926c-cbfe8f6c8a52  loglevel=3  systemd.show_status=true rw initrd=\EFI\debian\initrd.img-$kernel-debian" > /boot/efi/EFI/debian.nsh
+echo " \EFI\debian\vmlinuz-$kernel-debian root=PARTUUID=ad5ef658-ccc9-46a5-8363-107a8e5e7d15  loglevel=3  systemd.show_status=true rw initrd=\EFI\debian\initrd.img-$kernel-debian" > /boot/efi/EFI/debian.nsh
 
 cat /boot/efi/EFI/debian.nsh
 
